999f3ca1fd2b638162f7343cdebf441fd7ce59d9
abe_clark <abeaclark@gmail.com>
2016-04-05 18:49:06 -0700

    delivering html back locally

--- api/resolver.js
+++ api/resolver.js
@@ -1,13 +1,33 @@
 module.exports = function(req, res) {
-  // console.log('req.body', req.body)
-  // var data = JSON.parse(req.body.params);
-  // if (!data) {
-  //   res.status(403  Unauthorized  ).send('Invalid params');
-  //   return;
-  // }
+
+  var data = req.body
+  console.log(data)
+
+  var apple_link = data['apple'] || ''
+  var google_link = data['google'] || ''
+  var windows_link = data['windows'] || ''
+  var amazon_link = data['amazon'] || ''
+
+  if (apple_link) {
+    apple_link = "<a href='" + apple_link + "' ><img src='http://mixmax-app-icon.herokuapp.com/img/apple.png'></a>"
+  }
+
+  if (google_link) {
+    google_link = "<a href='" + google_link + "' ><img src='http://mixmax-app-icon.herokuapp.com/img/apple.png'></a>"
+  }
+
+  if (windows_link) {
+    windows_link = "<a href='" + windows_link + "' ><img src='http://mixmax-app-icon.herokuapp.com/img/apple.png'></a>"
+  }
+
+  if (amazon_link) {
+    amazon_link = "<a href='" + amazon_link + "' ><img src='http://mixmax-app-icon.herokuapp.com/img/apple.png'></a>"
+  }
+
+  var html = "<div>" + apple_link + google_link + windows_link + amazon_link +"</div>"
 
   res.json({
-    body: 'got there'
+    body: html
     // Add raw:true if you're returning content that you want the user to be able to edit
   });
 };
