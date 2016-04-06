6eb957588ecf2ad0ec1e704f7bb45b3bc3dc2d62
abe_clark <abeaclark@gmail.com>
2016-04-06 10:06:40 -0700

    fix document.ready

--- public/js/editor.js
+++ public/js/editor.js
@@ -1,26 +1,26 @@
-(function(){
-
+$( document ).ready(function() {
   $('#main-form').on('submit', function(e){
     e.preventDefault();
+    console.log('clicked submit')
     done(e);
   });
 
 
   var done = function(e) {
-        var apple = $('#apple').next().val();
-        var google = $('#google').next().val();
-        var amazon = $('#amazon').next().val();
+    var apple = $('#apple').next().val();
+    var google = $('#google').next().val();
+    var amazon = $('#amazon').next().val();
 
-        // Let Mixmax know it was done.
-        Mixmax.done({
-          apple: apple,
-          google: google,
-          amazon: amazon,
-        });
-      }
+    // Let Mixmax know it was done.
+    Mixmax.done({
+      apple: apple,
+      google: google,
+      amazon: amazon,
+    });
+  };
 
   var cancel = function() {
-        Mixmax.cancel();
-  }
+    Mixmax.cancel();
+  };
 
-})
+});
